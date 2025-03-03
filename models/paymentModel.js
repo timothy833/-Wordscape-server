f66eee860cf4ca5cacc01e080208fed4e7d00a4d
lifaa12 <andy0312huang@gmail.com>
2025-03-04 00:32:27 +0800

    移除註解

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -1,27 +1,5 @@
 const db = require('../db');
 
-// exports.createPayment = async (user_id, amount, status) => {
-//   try {
-//     const result = await db.query(`
-//             INSERT INTO payments (id, user_id, amount, status, created_at)
-//             VALUES (gen_random_uuid(), $1, $2, $3, NOW()) RETURNING *;
-//         `, [user_id, amount, status]);
-//     return result.rows[0];
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-
-
-// exports.getPaymentsByUser = async (userId) => {
-//   try {
-//     const result = await db.query(`SELECT * FROM payments WHERE user_id = $1 ORDER BY created_at DESC;`, [userId]);
-//     return result.rows;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 exports.createPayment = async (user_id, receiver_id, amount) => {
   try {
     const result = await db.query(`
