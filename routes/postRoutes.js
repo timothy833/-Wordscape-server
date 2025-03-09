4e32a98918bf24edb3e9fb5cd095fda28d340224
timothy833 <ca9m8e5zy@gmail.com>
2025-03-09 18:38:41 +0800

    代理圖片路由被關掉修正

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -59,7 +59,7 @@ router.post("/upload/content", authMiddleware, postController.uploadContentImage
 
 
 // ✅ 代理圖片（Cloudflare Cache）
-// router.get("/proxy/image", postController.proxyImage);
+router.get("/proxy/image", postController.proxyImage);
 
 
 module.exports = router;
